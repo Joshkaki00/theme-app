67537d16d490cb9c81f071cb0f97492b773e9406
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-02-19 17:31:08 -0800

    Fix: Add fonts property to NavigationContainer theme

--- App.js
+++ App.js
@@ -32,6 +32,24 @@ export default function App() {
             border: theme.colors.border,
             notification: theme.colors.primary,
           },
+          fonts: {
+            regular: {
+              fontFamily: 'System',
+              fontWeight: '400',
+            },
+            medium: {
+              fontFamily: 'System',
+              fontWeight: '500',
+            },
+            bold: {
+              fontFamily: 'System',
+              fontWeight: '700',
+            },
+            heavy: {
+              fontFamily: 'System',
+              fontWeight: '800',
+            },
+          },
         }}
       >
       <Tab.Navigator
